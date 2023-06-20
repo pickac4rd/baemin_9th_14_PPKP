2d8782b634074e802ace104201dc106e16c8189f
sgkim <kimsg64090@gmail.com>
2023-06-20 21:33:06 +0900

    first commit

---
+++ src/main/java/baemin/ppkp/Baemin9th14Ppkp2Application.java
@@ -0,0 +1,13 @@
+package baemin.ppkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baemin9th14Ppkp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baemin9th14Ppkp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/baemin/ppkp/Baemin9th14Ppkp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package baemin.ppkp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baemin9th14Ppkp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
